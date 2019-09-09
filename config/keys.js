ac5a965155bfb6bf0d9f8dc40204ba4a2118c91e
David Chen <davidchen3501@gmail.com>
2019-09-09 09:46:24 +0800

    update mongodb database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://localhost/1';
+dbPassword = 'mongodb+srv://daivd:<421173464>@cluster0-lollz.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     MongoURI: dbPassword
